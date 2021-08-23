4fb339abe1e7cf30f1fc8a4ec9bca735e73b2d39
DESKTOP-BSK5N4E\Lenovo <evgeni.drazhev@estafet.com>
2021-08-24 01:01:04 +0300

    cleaning

--- src/com/estafet/learning/Order.java
+++ src/com/estafet/learning/Order.java
@@ -1,6 +1,8 @@
 package com.estafet.learning;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 abstract public class Order {
@@ -8,12 +10,15 @@ abstract public class Order {
     private int orderNumber;
     private String clientDetails;
     private List<String> articles;
+    private String[] originCountry;
+    private String[] destinationCountry;
     private double totalAmount;
     private double totalAmountBeforeVAT;
     private double totalAmountBeforeVATWithDiscount;
     private double discountedAmount;
     private double totalAmountAfterVAT;
 
+    public String[] countries = new String[]{"Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra", "Angola", "Anguilla", "Antarctica", "Antigua and Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegowina", "Botswana", "Bouvet Island", "Brazil", "British Indian Ocean Territory", "Brunei Darussalam", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central African Republic", "Chad", "Chile", "China", "Christmas Island", "Cocos (Keeling) Islands", "Colombia", "Comoros", "Congo", "Congo, the Democratic Republic of the", "Cook Islands", "Costa Rica", "Cote d'Ivoire", "Croatia (Hrvatska)", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Falkland Islands (Malvinas)", "Faroe Islands", "Fiji", "Finland", "France", "France Metropolitan", "French Guiana", "French Polynesia", "French Southern Territories", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Heard and Mc Donald Islands", "Holy See (Vatican City State)", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran (Islamic Republic of)", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Korea, Democratic People's Republic of", "Korea, Republic of", "Kuwait", "Kyrgyzstan", "Lao, People's Democratic Republic", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libyan Arab Jamahiriya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia, The Former Yugoslav Republic of", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Martinique", "Mauritania", "Mauritius", "Mayotte", "Mexico", "Micronesia, Federated States of", "Moldova, Republic of", "Monaco", "Mongolia", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Niue", "Norfolk Island", "Northern Mariana Islands", "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Pitcairn", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russian Federation", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Seychelles", "Sierra Leone", "Singapore", "Slovakia (Slovak Republic)", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Georgia and the South Sandwich Islands", "Spain", "Sri Lanka", "St. Helena", "St. Pierre and Miquelon", "Sudan", "Suriname", "Svalbard and Jan Mayen Islands", "Swaziland", "Sweden", "Switzerland", "Syrian Arab Republic", "Taiwan, Province of China", "Tajikistan", "Tanzania, United Republic of", "Thailand", "Togo", "Tokelau", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks and Caicos Islands", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "United States Minor Outlying Islands", "Uruguay", "Uzbekistan", "Vanuatu", "Venezuela", "Vietnam", "Virgin Islands (British)", "Virgin Islands (U.S.)", "Wallis and Futuna Islands", "Western Sahara", "Yemen", "Yugoslavia", "Zambia", "Zimbabwe", "Palestine"};
 
 
     private static final int MAX_NUMBER_OF_ORDERS_PER_CLIENT;
@@ -22,32 +27,9 @@ abstract public class Order {
         MAX_NUMBER_OF_ORDERS_PER_CLIENT = 10000;
     }
 
-
-    Order() {
-        System.out.println("I am an Order() constructor.");
+    public Order() {
     }
 
-
-    Order(int orderNumber, double totalAmount) {
-        System.out.println("I am an Order(int orderNumber, double totalAmount) constructor.");
-    }
-
-
-    Order(String clientDetails, List<String> articles) {
-        this.clientDetails = clientDetails;
-        this.articles = articles;
-        System.out.println("I am an Order(String[] clientDetails, List<String> articles) constructor.");
-    }
-
-
-    // calling the following constructor: Order(String[] clientDetails, List<String> articles)
-    Order(int orderNumber, String clientDetails, List<String> articles, double totalAmount) {
-        this.orderNumber = orderNumber;
-        this.clientDetails = clientDetails;
-        this.articles = articles;
-        this.totalAmount = totalAmount;
-        System.out.println("I am an Order(int orderNumber, String[] clientDetails, List<String> articles, double totalAmount) constructor.");
-    }
 
     public static void printObjectPropertiesStatic(List<Order> ordersObjects) {
         for (Order item : ordersObjects) {
@@ -59,6 +41,94 @@ abstract public class Order {
             System.out.println("Item: " + this.toString());
     }*/
 
+
+    public int getOrderNumber() {
+        return orderNumber;
+    }
+
+    public void setOrderNumber(int orderNumber) {
+        this.orderNumber = orderNumber;
+    }
+
+    public String getClientDetails() {
+        return clientDetails;
+    }
+
+    public void setClientDetails(String clientDetails) {
+        this.clientDetails = clientDetails;
+    }
+
+    public List<String> getArticles() {
+        return articles;
+    }
+
+    public void setArticles(List<String> articles) {
+        this.articles = articles;
+    }
+
+    public String[] getOriginCountry() {
+        return originCountry;
+    }
+
+    public void setOriginCountry(String[] originCountry) {
+        this.originCountry = originCountry;
+    }
+
+    public String[] getDestinationCountry() {
+        return destinationCountry;
+    }
+
+    public void setDestinationCountry(String[] destinationCountry) {
+        this.destinationCountry = destinationCountry;
+    }
+
+    public double getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public double getTotalAmountBeforeVAT() {
+        return totalAmountBeforeVAT;
+    }
+
+    public void setTotalAmountBeforeVAT(double totalAmountBeforeVAT) {
+        this.totalAmountBeforeVAT = totalAmountBeforeVAT;
+    }
+
+    public double getTotalAmountBeforeVATWithDiscount() {
+        return totalAmountBeforeVATWithDiscount;
+    }
+
+    public void setTotalAmountBeforeVATWithDiscount(double totalAmountBeforeVATWithDiscount) {
+        this.totalAmountBeforeVATWithDiscount = totalAmountBeforeVATWithDiscount;
+    }
+
+    public double getDiscountedAmount() {
+        return discountedAmount;
+    }
+
+    public void setDiscountedAmount(double discountedAmount) {
+        this.discountedAmount = discountedAmount;
+    }
+
+    public double getTotalAmountAfterVAT() {
+        return totalAmountAfterVAT;
+    }
+
+    public void setTotalAmountAfterVAT(double totalAmountAfterVAT) {
+        this.totalAmountAfterVAT = totalAmountAfterVAT;
+    }
+
+    public String[] getCountries() {
+        return countries;
+    }
+
+    public void setCountries(String[] countries) {
+        this.countries = countries;
+    }
 
     @Override
     public boolean equals(Object obj)
